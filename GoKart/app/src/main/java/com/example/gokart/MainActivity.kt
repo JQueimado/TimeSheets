d877b8df01707f6bed76e00dcba3b164814e5833
JQueimado <jpqueimado@gmail.com>
2021-04-26 02:11:17 +0100

    Finished Redo Redo

--- GoKart/app/src/main/java/com/example/gokart/MainActivity.kt
+++ GoKart/app/src/main/java/com/example/gokart/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.gokart
 
+import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<StatsFragment>(R.id.stats_fragment_container_view)
+            }
+        }
+
     }
 }
