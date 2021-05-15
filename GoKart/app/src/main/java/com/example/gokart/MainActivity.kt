68b089111cdebd4e15c3988dbb5a81032ef806b7
JQueimado <jpqueimado@gmail.com>
2021-05-15 19:07:03 +0100

    UI inprovements

--- GoKart/app/src/main/java/com/example/gokart/MainActivity.kt
+++ GoKart/app/src/main/java/com/example/gokart/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.example.gokart
 
-import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
+import android.text.format.Time
+import android.widget.LinearLayout
+import androidx.fragment.app.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<StatsFragment>(R.id.stats_fragment_container_view)
+                add<TimeSheetFragment>(R.id.times_sheet_fragment_container_view)
             }
         }
+
+        val timesheetlayout : LinearLayout = findViewById(R.id.times_sheet_list_container)
 
     }
 }
---
+++ GoKart/app/src/main/java/com/example/gokart/TimeSheetFragment.kt
@@ -0,0 +1,52 @@
+package com.example.gokart
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class TimeSheetFragment : Fragment(R.layout.fragment_table) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if(savedInstanceState == null){
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+
+        var timeSheet : TableLayout = view!!.findViewById(R.id.times_sheet)
+
+        var temp : View = addRow(1, "1.1.1111", "+1.111", "+1")
+        timeSheet.addView(temp)
+
+        temp = addRow(2, "1.1.1111", "+1.111", "+1")
+        timeSheet.addView(temp)
+
+        return view
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun addRow (lap : Int, time : String, bestlap_delta : String, last_delta : String ) : View{
+        val row : View = layoutInflater.inflate(R.layout.timesheetrow, null)
+
+        row.findViewById<TextView>(R.id.time_sheet_lap_number).setText(lap.toString())
+        row.findViewById<TextView>(R.id.time_sheet_lap).setText(time)
+        row.findViewById<TextView>(R.id.time_sheet_bestlap_delta).setText(bestlap_delta)
+        row.findViewById<TextView>(R.id.time_sheet_lastlap_delta).setText(last_delta)
+
+        return row
+    }
+
+}
