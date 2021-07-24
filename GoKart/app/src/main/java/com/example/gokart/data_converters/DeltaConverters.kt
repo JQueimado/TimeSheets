64bdb45af66e3b2b1a914e5263c130092b5a6d6d
JQueimado <jpqueimado@gmail.com>
2021-07-24 20:45:38 +0100

    Implement and test delta converters

--- GoKart/app/src/main/java/com/example/gokart/data_converters/DeltaConverters.kt
+++ GoKart/app/src/main/java/com/example/gokart/data_converters/DeltaConverters.kt
@@ -1,7 +1,42 @@
 package com.example.gokart.data_converters
 
-/// delta format +/-M?:SS.ssss
+/// delta format +/-M?:SS.sss
 
 fun String.toIntDelta() : Int {
-    return 0
+    val min: Int
+    val sec: Int
+    val mls: Int
+    val signal = this[0]
+    val text = this.substring(1,this.length)
+
+    val split1 = text.split(":")
+    val split2 : List<String>?
+    if ( split1.size != 1 ) {
+        min = split1[0].toInt()
+        split2 = split1[1].split(".")
+    }else{
+        min = 0
+        split2 = split1[0].split(".")
+    }
+
+    sec = split2[0].toInt()
+    mls = split2[1].toInt()
+
+    return (if(signal == '-' ) -1 else 1 ) * (((( min * 60 ) + sec ) * 1000 ) + mls)
+}
+
+fun Int.toStringDelta() : String{
+    val value = if( this < 0  ) -1*this else this
+
+    val temp = value / 1000
+    val mls = value % 1000
+    val sec = temp % 60
+    val min = temp / 60
+
+    val format = if( min == 0 )
+        "%d.%03d".format( sec,mls )
+    else
+        "%d:%02d.%03d".format( min,sec,mls )
+
+    return if( this < 0 ) "-$format" else "+$format"
 }
--- GoKart/app/src/test/java/com/example/gokart/DeltaConverterTests.kt
+++ GoKart/app/src/test/java/com/example/gokart/DeltaConverterTests.kt
@@ -1,4 +1,70 @@
 package com.example.gokart
 
+import com.example.gokart.data_converters.toIntDelta
+import com.example.gokart.data_converters.toStringDelta
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+
 class DeltaConverterTests {
+    private val text = arrayListOf(
+        "-1:18.348",
+        "+1:18.355",
+        "-1:18.746",
+        "+1:18.890",
+        "-1:19.039",
+        "+1:19.042",
+        "-1:19.116",
+        "+1:19.306",
+        "-1:19.479",
+        "+1:19.659",
+        "-56.157"
+    )
+
+    private val number = arrayListOf(
+        -78348,
+        +78355,
+        -78746,
+        +78890,
+        -79039,
+        +79042,
+        -79116,
+        +79306,
+        -79479,
+        +79659,
+        -56157
+    )
+
+    @Test
+    fun toNumberTest(){
+        for ( i in 0 until text.size ){
+            MatcherAssert.assertThat(text[i].toIntDelta(), CoreMatchers.equalTo(number[i]))
+        }
+    }
+
+    @Test
+    fun toTextTest(){
+        for ( i in 0 until number.size ){
+            MatcherAssert.assertThat(number[i].toStringDelta(), CoreMatchers.equalTo(text[i]))
+        }
+    }
+
+    @Test
+    fun backAndForwardTest(){
+        var temp: Int
+        for ( i in 0 until text.size ){
+            temp = text[i].toIntDelta()
+            MatcherAssert.assertThat(temp.toStringDelta(), CoreMatchers.equalTo(text[i]))
+        }
+    }
+
+    @Test
+    fun reverseBackAndForwardTest(){
+        var temp: String
+        for ( i in 0 until number.size ){
+            temp = number[i].toStringDelta()
+            MatcherAssert.assertThat(temp.toIntDelta(), CoreMatchers.equalTo(number[i]))
+        }
+    }
+
 }
