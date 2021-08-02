56a287472de6418cafb8d5b0b864b3bd05e1288a
JQueimado <jpqueimado@gmail.com>
2021-08-02 16:50:57 +0100

    Removed Database trash

--- GoKart/app/src/main/java/com/example/gokart/database/entity/StatsEntity.kt
+++ GoKart/app/src/main/java/com/example/gokart/database/entity/StatsEntity.kt
@@ -10,9 +10,8 @@ data class StatsEntity(
     @ColumnInfo(name = "stats_id") val id: Long = 0,
     @ColumnInfo(name = "stats_sheet_best_lap") var bestLap: Int,
     @ColumnInfo(name = "stats_sheet_average_lap_sum") var averageLapSum: Long,
-    @ColumnInfo(name = "stats_sheet_average_lap_num") var averageLapNum: Long,
     @ColumnInfo(name = "stats_sheet_consistency_sum") var consistencySum: Long,
-    @ColumnInfo(name = "stats_sheet_consistency_num") var consistencyNum: Long,
+    @ColumnInfo(name = "stats_sheet_entries") var entryNumber: Int,
     @ColumnInfo(name = "stats_sheet_favourite_kart") var favouriteKart: Long,
     @ColumnInfo(name = "stats_sheet_favourite_karting_center") var favouriteKartingCenter: Long,
 )
