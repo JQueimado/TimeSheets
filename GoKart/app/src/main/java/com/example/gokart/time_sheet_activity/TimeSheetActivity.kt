8731e650bad22e70414005d985c18bb7e21be72a
JQueimado <jpqueimado@gmail.com>
2021-08-03 18:55:01 +0100

    Add TimeSheetActivity

---
+++ GoKart/app/src/main/java/com/example/gokart/time_sheet_activity/TimeSheetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gokart.time_sheet_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gokart.R
+
+class TimeSheetActivity : AppCompatActivity(R.layout.activity_time_sheet) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
