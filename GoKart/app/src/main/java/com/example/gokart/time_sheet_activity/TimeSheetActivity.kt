96ba9761ce0f3dd2f2105edaf59eb71cb29d16ac
JQueimado <jpqueimado@gmail.com>
2021-08-03 20:09:30 +0100

    Add basic UI for time sheet activity

--- GoKart/app/src/main/java/com/example/gokart/time_sheet_activity/TimeSheetActivity.kt
+++ GoKart/app/src/main/java/com/example/gokart/time_sheet_activity/TimeSheetActivity.kt
@@ -2,11 +2,22 @@ package com.example.gokart.time_sheet_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.gokart.R
 
 class TimeSheetActivity : AppCompatActivity(R.layout.activity_time_sheet) {
+
+    companion object{
+        private const val backButtonId = R.id.nav_back_button
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Back
+        findViewById<Button>(backButtonId).setOnClickListener {
+            finish()
+        }
 
     }
 }
